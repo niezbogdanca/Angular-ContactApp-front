013090c836c3657460aac9970e6543be07d7251b
Maciek Skrzynski <maciek9764@interia.pl>
2023-05-27 12:19:00 +0200

    add data CONTACTS

---
+++ src/app/data/contacts-data.ts
@@ -0,0 +1,6 @@
+export const CONTACTS = [
+    {id: 1, surname: 'Nowak', firstName: 'Tomasz', phoneNumber: 123456},
+    {id: 2, surname: 'Nowakowska', firstName: 'Anna', phoneNumber: 7219178432},
+    {id: 3, surname: 'Kowalski', firstName: 'Jan', phoneNumber: 99387251 },
+    {id: 4, surname: 'Kowal', firstName: 'Jan', phoneNumber: 9932364251 },
+]
